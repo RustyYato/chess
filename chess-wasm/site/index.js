93e84f2054b1cda9ffbcacca5c304a863dca3d8e
RustyYato <rustyyato@gmail.com>
2023-07-23 16:35:01 -0700

    create a basic wasm example

--- chess-wasm/site/index.js
+++ chess-wasm/site/index.js
@@ -1,3 +1,20 @@
+let engine = null;
+let game = null;
+
 import("./node_modules/chess-wasm/chess_wasm.js").then((js) => {
-    js.greet("WebAssembly with npm");
+    js.init_logging();
+
+    if (engine === null) {
+        engine = js.new_engine();
+    }
+
+    if (game === null) {
+        game = js.new_game();
+    }
+
+    console.log("search for a move");
+
+    const move = engine.search(game, "100microseconds");
+
+    console.log("found a move: ", move.chess_move());
 });
--- chess-wasm/src/lib.rs
+++ chess-wasm/src/lib.rs
@@ -1,11 +1,89 @@
+use std::time::Duration;
+
+use chess_movegen::ChessMove;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub struct ChessEngine {
+    engine: chess_engine::Engine,
 }
 
 #[wasm_bindgen]
-pub fn greet(s: &str) {
-    alert(&format!("hello world {s}"))
+pub struct ChessGame {
+    history: chess_engine::ThreeFold,
+    board: chess_movegen::Board,
+}
+
+#[wasm_bindgen]
+pub struct EngineChessMove {
+    chess_move: Option<ChessMove>,
+    score: chess_engine::Score,
+}
+
+#[wasm_bindgen]
+impl ChessEngine {
+    pub fn search(
+        &mut self,
+        game: &ChessGame,
+        timeout: Option<String>,
+    ) -> Result<EngineChessMove, wasm_bindgen::JsError> {
+        let timeout = timeout
+            .map(|timeout| parse_duration::parse(&timeout))
+            .transpose()
+            .map_err(|err| wasm_bindgen::JsError::new(&err.to_string()))?
+            .unwrap_or(Duration::from_secs(5));
+
+        let (chess_move, score) = self.engine.search(
+            &game.board,
+            &game.history,
+            chess_engine::DurationTimeout::new(timeout),
+        );
+
+        Ok(EngineChessMove { chess_move, score })
+    }
+}
+
+#[wasm_bindgen]
+impl EngineChessMove {
+    pub fn chess_move(&self) -> Option<String> {
+        self.chess_move.map(|mv| mv.to_string())
+    }
+}
+
+use tracing_wasm::{set_as_global_default_with_config, WASMLayerConfigBuilder};
+
+#[wasm_bindgen]
+pub fn init_logging() {
+    console_error_panic_hook::set_once();
+    set_as_global_default_with_config(
+        WASMLayerConfigBuilder::new()
+            .set_max_level(tracing::Level::DEBUG)
+            .build(),
+    )
+}
+
+#[wasm_bindgen]
+pub fn new_engine() -> ChessEngine {
+    ChessEngine {
+        engine: chess_engine::Engine::default(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn new_game() -> ChessGame {
+    ChessGame {
+        history: chess_engine::ThreeFold::new(),
+        board: chess_movegen::Board::standard(),
+    }
+}
+
+#[wasm_bindgen]
+pub fn new_game_from_fen(s: &str) -> Result<ChessGame, JsError> {
+    let board = s
+        .parse::<chess_movegen::Board>()
+        .map_err(|err| JsError::new(&format!("{err:?}")))?;
+    Ok(ChessGame {
+        history: chess_engine::ThreeFold::new(),
+        board,
+    })
 }
